5ec6f05b30b505942c4da0a59824471abc44cc18
Dawid Rynkal <dawid@epicup.pl>
2021-12-02 18:46:55 +0100

    styled components

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { users } from 'data/users';
-import UsersListItem from 'components/UsersListItem/UsersListItem';
+import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import PropTypes from 'prop-types';
+import { Wrapper } from './UserList.styles';
 
 const UsersList = () => (
-  <div>
+  <Wrapper>
     <ul>
       {users.map((userData) => (
         <UsersListItem userData={userData} />
       ))}
     </ul>
-  </div>
+  </Wrapper>
 );
 
 UsersList.propTypes = {};
