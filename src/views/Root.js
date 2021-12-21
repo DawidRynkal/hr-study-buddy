ac05d606ab337257840d47c4170353398c508624
Dawid Rynkal <dawid@epicup.pl>
2021-12-21 17:46:00 +0100

    axios posts

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
-import { theme } from 'assets/styles/themes';
+import { theme } from 'assets/styles/theme';
 import { Wrapper } from './Root.styles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
